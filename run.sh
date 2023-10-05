6a39fbaddea9d63832ebd888118b9058547292a3
Charlie Hagedorn <charlie@hagedorn.xyz>
2023-10-05 13:18:45 -0600

    Comments out successful updating script.

--- run.sh
+++ run.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 touch ~/it_works_for_real.txt
 
-cp ~/acquire.sh ~/acquire-`date`
-cp ~/booster-cmd/acquire.sh.prototype ~/acquire.sh
-chmod a+x ~/acquire.sh
+#Uncomment below to update the acquire script from the repo prototype.
+#cp ~/acquire.sh ~/acquire-`date`
+#cp ~/booster-cmd/acquire.sh.prototype ~/acquire.sh
+#chmod a+x ~/acquire.sh
