9c4cf6071363ddd48b90aa5c3aca12d0c98c1681
Charlie Hagedorn <charlie@hagedorn.xyz>
2024-10-09 15:13:15 -0700

    Update run.sh in an attempt to un-hork operations, October 2022
    Adding a call to remove images from 2022, in case the issue is disk-space.

--- run.sh
+++ run.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 touch ~/it_works_for_real.txt
+rm ~/cam1/2022*.jpg
+rm ~/cam2/2022*.jpg
 
 #Uncomment below to update the acquire script from the repo prototype.
 #cp ~/acquire.sh ~/acquire-`date`
