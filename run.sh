1f65b497075d40349cb261bc3f7774495f2b93ae
Charlie Hagedorn <charlie@hagedorn.xyz>
2024-10-09 15:37:55 -0700

    Adds disk-space monitoring

--- run.sh
+++ run.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 touch ~/it_works_for_real.txt
-rm ~/cam1/2022*.jpg
-rm ~/cam2/2022*.jpg
+
+df -h --total > diskspace.dat
+rclone copy diskspace.dat gdrive:
+
+#rm ~/cam1/2022*.jpg
+#rm ~/cam2/2022*.jpg
 
 #Uncomment below to update the acquire script from the repo prototype.
 #cp ~/acquire.sh ~/acquire-`date`
