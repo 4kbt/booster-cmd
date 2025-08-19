5a9c9277ee8f0c7b88db717485855a41258086a1
Charlie Hagedorn <charlie@hagedorn.xyz>
2025-08-18 18:32:31 -0600

    Update run.sh to remove 2024's images, too.

--- run.sh
+++ run.sh
@@ -13,8 +13,11 @@ rclone copy diskspace.dat gdrive:
 
 #rm ~/cam1/2022*.jpg
 #rm ~/cam2/2022*.jpg #Already executed, January 2025
-rm ~/cam1/2023*.jpg
-rm ~/cam2/2023*.jpg #Attempting to execute, August 2025
+#rm ~/cam1/2023*.jpg
+#rm ~/cam2/2023*.jpg #Attempting to execute, August 2025
+rm ~/cam1/2024*.jpg
+rm ~/cam2/2024*.jpg #Attempting to execute, August 2025
+
 
 
 #Disk-space tracking
